6f9f0b6d5ac7e9a3e4bee95da96fdb593bd5ec71
Johannes Brück <6677058+bruecktech@users.noreply.github.com>
2019-10-07 17:19:42 +0200

    Move scripts to typescript file, call synth on CtrlCmd+S

--- web/index.ts
+++ web/index.ts
@@ -73,8 +73,75 @@ window.editor = monaco.editor.create(document.getElementById('editor'), {
   theme: 'vs-dark'
 });
 
+window.editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, function() {
+	synth();
+});
+
 window.output = monaco.editor.create(document.getElementById('output'), {
   language: "yaml",
   theme: 'vs-dark',
   readOnly: true
 });
+
+
+const params = new URLSearchParams(document.location.search);
+const hash = params.get("s");
+if(hash){
+    load_code(hash);
+    load_cf(hash);
+}
+
+function alert(type, message){
+  document.getElementById("alert").classList.value = "alert alert-" + type;
+  document.getElementById("alert").innerHTML = message.replace(/\n/g, "<br />");
+}
+
+function synth() {
+  document.getElementById("spinner").style.visibility = "visible"
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          const response = JSON.parse(this.responseText);
+          window.output.setValue(response.cf_template);
+          document.getElementById("share_code").innerHTML = '<a href="https://play-with-cdk.com?s=' + response.share_code + '">share</a>'
+          document.getElementById("deploy").innerHTML = '<a target="_blank" href="https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/new?stackName=myteststack&templateURL=https://s3-eu-west-1.amazonaws.com/play-with-cdk.com/shared/' + response.share_code + '_cf"><img src="https://s3.amazonaws.com/cloudformation-examples/cloudformation-launch-stack.png"></a>'
+          document.getElementById("spinner").style.visibility = "hidden";
+          alert("success", "Synth successful");
+      }
+      if (this.readyState == 4 && this.status != 200) {
+          document.getElementById("spinner").style.visibility = "hidden";
+          try {
+              const response = JSON.parse(this.responseText);
+              alert("danger", response.error + "\n" + response.details.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+          } catch(err) {
+              alert("danger", this.responseText);
+          }
+      }
+  };
+  xhttp.open("POST", "https://fvml9pequc.execute-api.eu-west-1.amazonaws.com/prod/synth", true);
+  xhttp.send(window.editor.getValue());
+}
+
+function load_cf(hash) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          window.output.setValue(this.responseText);
+          document.getElementById("deploy").innerHTML = '<a target="_blank" href="https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/new?stackName=myteststack&templateURL=https://s3-eu-west-1.amazonaws.com/play-with-cdk.com/shared/' + hash + '_cf"><img src="https://s3.amazonaws.com/cloudformation-examples/cloudformation-launch-stack.png"></a>'
+      }
+  };
+  xhttp.open("GET", "https://play-with-cdk.com/shared/" + hash + '_cf', true);
+  xhttp.send();
+}
+
+function load_code(hash) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          window.editor.setValue(this.responseText);
+          document.getElementById("share_code").innerHTML = '<a href="https://play-with-cdk.com?s=' + hash + '">share</a>'
+      }
+  };
+  xhttp.open("GET", "https://play-with-cdk.com/shared/" + hash + '_code', true);
+  xhttp.send();
+}
