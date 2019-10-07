e06ac4de53fde72239bc6820f2a93249d36a528a
Johannes Brück <6677058+bruecktech@users.noreply.github.com>
2019-10-07 19:45:44 +0200

    Auto generate typings

--- web/generate_typings.sh
+++ web/generate_typings.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-cd $1
-rm out.js
-echo "exports.alltypes = [" >> out.js
+mkdir tmp
+cp ../lambda/package.json tmp
+cp ../lambda/package-lock.json tmp
+pushd tmp
+npm ci
+pushd "node_modules/@aws-cdk"
+echo "exports.alltypes = [" >> typings.js
 for file in `find * \( -name package.json -o -name "*.d.ts" \) -print`
 do
   echo $file
-  echo "{ name: 'file:///$1/$file', value: \`$(cat $file | base64 )\` }," >> out.js
+  echo "{ name: 'file:///$1/$file', value: \`$(cat $file | base64 )\` }," >> typings.js
 done
-echo "]" >> out.js
+echo "]" >> typings.js
+popd
+popd
+mv "tmp/node_modules/@aws-cdk/typings.js" .
+rm -rf tmp
