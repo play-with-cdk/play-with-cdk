fd8fdcfe0fda55907d34898223d5e5ec17a59808
Johannes Brück <6677058+bruecktech@users.noreply.github.com>
2019-10-22 11:47:35 +0200

    Replace node-prune with own script to avoid deletion of images and assets folders

--- deploy/lib/pipeline-stack.ts
+++ deploy/lib/pipeline-stack.ts
@@ -58,8 +58,7 @@ export class PipelineStack extends Stack {
         phases: {
           install: {
             "runtime-versions": {
-              nodejs: 10,
-              docker: 18
+              nodejs: 10
             },
             commands: [
               'cd lambda',
@@ -69,7 +68,75 @@ export class PipelineStack extends Stack {
           build: {
             commands: [
               'npm run build',
-              'docker run --rm -v ${PWD}:/app hochzehn/node-prune'
+              `find . -type d -name node_modules -prune -exec find {} -type f \( \
+                -iname Makefile -or \
+                -iname README -or \
+                -iname README.md -or \
+                -iname CHANGELOG -or \
+                -iname CHANGELOG.md -or \
+                -name .editorconfig -or \
+                -name .gitmodules -or \
+                -name .gitattributes -or \
+                -name robot.html -or \
+                -name .lint -or \
+                -iname Gulpfile.js -or \
+                -iname Gruntfile.js -or \
+                -name .tern-project -or \
+                -name .gitattributes -or \
+                -name .editorconfig -or \
+                -name .eslintrc -or \
+                -name .jshintrc -or \
+                -name .npmignore -or \
+                -name .flowconfig -or \
+                -name .documentup.json -or \
+                -name .yarn-metadata.json -or \
+                -name .travis.yml -or \
+                -name thumbs.db -or \
+                -name .tern-port -or \
+                -name .ds_store -or \
+                -name desktop.ini -or \
+                -name npm-debug.log -or \
+                -name .npmrc -or \
+                -iname LICENSE.txt -or \
+                -iname LICENSE.md -or \
+                -iname LICENSE-MIT -or \
+                -iname LICENSE-MIT.txt -or \
+                -iname LICENSE.BSD -or \
+                -iname LICENSE-BSD -or \
+                -iname LICENSE-jsbn -or \
+                -iname LICENSE -or \
+                -iname AUTHORS -or \
+                -iname CONTRIBUTORS -or \
+                -name .yarn-integrity -or \
+                -name builderror.log -or \
+                -name "*.md" -or \
+                -name "*.sln" -or \
+                -name "*.obj" -or \
+                -name "*.gypi" -or \
+                -name "*.vcxproj" -or \
+                -name "*.vcxproj.filters" -or \
+                \( -name '*.ts' -and \! -name '*.d.ts' \) -or \
+                -name "*.jst" -or \
+                -name "*.coffee" \
+              \) -print0 \; | xargs -0 rm -rf
+
+            # Common unneeded directories
+            find . -type d -name node_modules -prune -exec find {} -type d \( \
+                -name __tests__ -or \
+                -name test -or \
+                -name tests -or \
+                -name powered-test -or \
+                -name docs -or \
+                -name doc -or \
+                -name website -or \
+                -name example -or \
+                -name examples -or \
+                -name coverage -or \
+                -name node-gyp -or \
+                -name node-pre-gyp -or \
+                -name gyp -or \
+                -name .nyc_output \
+              \) -print0 \; | xargs -0 rm -rf`
             ]
           },
         },
@@ -85,8 +152,7 @@ export class PipelineStack extends Stack {
         },
       }),
       environment: {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2,
-        privileged: true
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2
       },
     });
 
